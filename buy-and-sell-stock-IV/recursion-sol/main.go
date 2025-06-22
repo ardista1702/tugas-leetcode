159784e06c825d2e67e697a2245c29c4c4a8fc07
ardista1702 <ardist.1702@gmail.com>
2025-06-22 22:59:59 +0800

    ganti contoh soal

--- buy-and-sell-stock-IV/recursion-sol/main.go
+++ buy-and-sell-stock-IV/recursion-sol/main.go
@@ -2,37 +2,35 @@ package main
 
 import "fmt"
 
-
-
-func max(a,b int)int {
+func max(a, b int) int {
 	if a > b {
-		return  a
+		return a
 	}
-	return  b
+	return b
 }
 
-func backtrack(prices []int,day int,k int,holding bool) int {
+func backtrack(prices []int, day int, k int, holding bool) int {
 	if day == len(prices) || k == 0 {
 		return 0
 	}
-	if holding{
-		skipToday := backtrack(prices,day + 1,k,true) //profit ketika kita melewatkan hari ini
-		sellToday := prices[day] + backtrack(prices,day + 1,k-1,false)
-		return  max(skipToday,sellToday)
-	}else{
-		skipToday := backtrack(prices,day + 1,k,false)
-		buyToday := -prices[day] + backtrack(prices,day + 1,k,true)
-		return  max(skipToday,buyToday)
+	if holding {
+		skipToday := backtrack(prices, day+1, k, true) //profit ketika kita melewatkan hari ini
+		sellToday := prices[day] + backtrack(prices, day+1, k-1, false)
+		return max(skipToday, sellToday)
+	} else {
+		skipToday := backtrack(prices, day+1, k, false)
+		buyToday := -prices[day] + backtrack(prices, day+1, k, true)
+		return max(skipToday, buyToday)
 	}
 }
 
-func maxProfit(prices []int ,k int) int {
-	return  backtrack(prices,0,k,false)
+func maxProfit(prices []int, k int) int {
+	return backtrack(prices, 0, k, false)
 }
 
 func main() {
-	prices := []int{3,2}
-	k := 1
-	res := maxProfit(prices,k)
+	prices := []int{3, 2, 6, 5, 0, 3}
+	k := 2
+	res := maxProfit(prices, k)
 	fmt.Println(res)
 }
